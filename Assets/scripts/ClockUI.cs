5d20a5897b2d79db8dc96ef875e827b700e0d63b
Jeff Kloosterman <jkloosterman@michiganlabs.com>
2015-04-18 18:42:01 -0400

    rough in UI elements

---
+++ Assets/scripts/ClockUI.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ClockUI : MonoBehaviour {
+
+    public GameObject clockHand;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+
+    public void SetRotation(float angle)
+    {
+        float current = clockHand.transform.rotation.z;
+        clockHand.transform.Rotate(0, 0, angle - current);
+    }
+}
