747b76a4b2330b7c8e971fb67c25e603fab1223d
Jeff Kloosterman <jkloosterman@michiganlabs.com>
2015-04-18 21:51:57 -0400

    move cursor control to player object

--- Assets/scripts/CursorControl.cs
+++ Assets/scripts/CursorControl.cs
@@ -15,36 +15,39 @@ public class CursorControl : MonoBehaviour {
     private SpriteRenderer spriteRenderer;
     private List<string> stoneBlocks;
     private Color playerColor;
+    private GameObject cursor;
+    private CountdownClock countdownClock;
 
     void Awake () {
         // TODO: get a reference to the countdown clock
-        body = this.GetComponent<Rigidbody2D>() as Rigidbody2D;
-        spriteRenderer = this.GetComponent<SpriteRenderer>() as SpriteRenderer;
 		//stoneBlocks = new List<string>{"stone/StoneColumn", "stone/StoneCircle", "stone/StoneLgTriangle", "stone/StoneTriangle", "stone/StoneSquare", "stone/StoneFlat"};
 		stoneBlocks = new List<string>{"stone/StoneColumn", "stone/StoneSquare", "stone/StoneFlat"};
+        cursor = transform.FindChild("cursor").gameObject;
+        body = cursor.GetComponent<Rigidbody2D>() as Rigidbody2D;
+        spriteRenderer = this.GetComponent<SpriteRenderer>() as SpriteRenderer;
+        countdownClock = transform.GetComponentInChildren<CountdownClock>();
 
-		if (playerNumber != 1) {
+        if (playerNumber != 1) {
             horizontalAxis = "Horizontal2";
             verticalAxis = "Vertical2";
             dropButton = "Drop2";
             playerColor = Color.red;
-            spriteRenderer.color = playerColor;
+            cursor.GetComponent<SpriteRenderer>().color = playerColor;
         } else {
             playerColor = Color.blue;
-            spriteRenderer.color = playerColor;
+            cursor.GetComponent<SpriteRenderer>().color = playerColor;
         }
     }
 
     void Start ()
     {
-
     }
 
     void Update ()
     {
 #if UNITY_EDITOR
-        float pX = transform.parent.position.x;
-        float pY = transform.parent.position.y;
+        float pX = cursor.transform.parent.position.x;
+        float pY = cursor.transform.parent.position.y;
         Debug.DrawLine(new Vector3(pX + topLeftBound.x, pY + bottomRightBound.y, 0), new Vector3(pX + bottomRightBound.x, pY + bottomRightBound.y, 0), playerColor);
         Debug.DrawLine(new Vector3(pX + topLeftBound.x, pY + topLeftBound.y, 0), new Vector3(pX + bottomRightBound.x, pY + topLeftBound.y, 0), playerColor);
         Debug.DrawLine(new Vector3(pX + bottomRightBound.x, pY + bottomRightBound.y, 0), new Vector3(pX + bottomRightBound.x, pY + topLeftBound.y, 0), playerColor);
@@ -56,32 +59,32 @@ public class CursorControl : MonoBehaviour {
 
         body.velocity = new Vector2 (hSpeed * maxSpeed, vSpeed * maxSpeed);
 
-        float newX = transform.localPosition.x;
-        float newY = transform.localPosition.y;
+        float newX = cursor.transform.localPosition.x;
+        float newY = cursor.transform.localPosition.y;
 
         // bound the cursor to the box
-        if (transform.localPosition.x <= topLeftBound.x) {
+        if (cursor.transform.localPosition.x <= topLeftBound.x) {
             newX = topLeftBound.x;
-        } else if (transform.localPosition.x >= bottomRightBound.x) {
+        } else if (cursor.transform.localPosition.x >= bottomRightBound.x) {
             newX = bottomRightBound.x;
         }
-        if (transform.localPosition.y >= topLeftBound.y) {
+        if (cursor.transform.localPosition.y >= topLeftBound.y) {
             newY = topLeftBound.y;
-        } else if(transform.localPosition.y <= bottomRightBound.y) {
+        } else if(cursor.transform.localPosition.y <= bottomRightBound.y) {
             newY = bottomRightBound.y;
         }
-        transform.localPosition = new Vector2 (newX, newY);
+        cursor.transform.localPosition = new Vector2 (newX, newY);
 
         // Droping blocks
         if (Input.GetButtonDown(dropButton)) {
             // TODO: delay between drops
             // TODO: reset countdown clock
-			transform.parent.GetComponentInChildren<CountdownClock>().ResetTimer();
+			countdownClock.ResetTimer();
 
             int r = Random.Range(0, stoneBlocks.Count);
             GameObject block = Instantiate(Resources.Load(stoneBlocks[r])) as GameObject;
-            block.transform.position = new Vector2(transform.position.x, transform.position.y);
-            block.transform.SetParent(transform.parent.transform);
+            block.transform.position = new Vector2(cursor.transform.position.x, cursor.transform.position.y);
+            block.transform.SetParent(transform);
         }
     }
 }
